ba35b48e380535b34ce4be079dfd0850f111a4b9
mwood881 <83769001+mwood881@users.noreply.github.com>
2025-03-09 23:22:16 -0500

    Update trimmean_test.go
    added some comments

--- trimmean_test.go
+++ trimmean_test.go
@@ -1,5 +1,5 @@
 // trimmean_test.go
-
+// use as bencmark testing
 package trimmean
 
 import (
@@ -13,16 +13,17 @@ func TestTrimmedMean(t *testing.T) {
 	if err != nil || mean != 5 {
 		t.Errorf("expected mean 5, got %v", mean)
 	}
-
+// Had to test asymmetric as well as it has to work for both assymetric and symmetric
 	// Test asymmetric trimming
 	mean, err = TrimmedMean(numbers, 0.1, 0.2)
 	if err != nil || mean != 5.5 {
 		t.Errorf("expected mean 5.5, got %v", mean)
 	}
 
-	// Test invalid trimming proportions (sum exceeds 1)
+	// Test is more than 1 so it is out of the trimming range
 	mean, err = TrimmedMean(numbers, 0.6, 0.6)
 	if err == nil {
 		t.Errorf("expected error for invalid trimming proportions, got mean %v", mean)
 	}
 }
+// ALL tests passed yayyy!
